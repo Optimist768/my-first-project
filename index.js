d754410aef4b178d97e6e5953e92679340b1252e
Optimist768 <tiarufai6@gmail.com>
2025-05-03 02:33:10 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const display = document.getElementById("display");
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error"
+    }
+}
